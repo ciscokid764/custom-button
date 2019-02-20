2b113d5a462459ed8be23967e152b376fe2ef99e
Ian Pilgrim <ipi38482@gmail.com>
2019-02-21 03:37:26 +0800

    Fix webpack error

--- custom-button/webpack.config.js
+++ custom-button/webpack.config.js
@@ -13,18 +13,16 @@ module.exports = {
       path: path.resolve(__dirname, 'dist')
    },
    module: {
-       rules: {
-           {
+       rules: [
                test: /\.js$/,
                exclude: /node_modules/,
                use: {
                    loader: 'babel-loader',
                    options: {
-                       presets: {"@babel/preset-env", "@babel/preset-react"}
+                       presets: {"@babel/preset-env": "@babel/preset-react"}
                    }
                }
-           }
-       }
+        ]
    },
    optimization: {
       minimizer: [new UglifyJsPlugin()]
